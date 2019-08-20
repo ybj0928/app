25440c04c53001528331f28c38a3e2b0ea7b6976
杨鞭骥 <yangbianji@palmyou.com>
2019-08-20 17:58:06 +0800

    提交

--- src/components/home/newsList.vue
+++ src/components/home/newsList.vue
@@ -1,11 +1,59 @@
 <template>
   <div>
-    新闻列表新闻列表新闻列表新闻列表新闻列表新闻列表新闻列表新闻列表新闻列表新闻列表新闻列表新闻列表新闻列表新闻列表新闻列表新闻列表新闻列表新闻列表新闻列表新闻列表新闻列表新闻列表新闻列表新闻列表新闻列表新闻列表
+    <ul class="mui-table-view">
+      <li class="mui-table-view-cell mui-media">
+        <a href="javascript:;">
+          <img class="mui-media-object mui-pull-left" src="../../static/images/1.jpg">
+          <div class="mui-media-body">
+            幸福
+            <p class='mui-ellipsis'>能和心爱的人一起睡觉，是件幸福的事情；可是，打呼噜怎么办？</p>
+          </div>
+        </a>
+      </li>
+      <li class="mui-table-view-cell mui-media">
+        <a href="javascript:;">
+          <img class="mui-media-object mui-pull-left" src="../../static/images/1.jpg">
+          <div class="mui-media-body">
+            木屋
+            <p class='mui-ellipsis'>想要这样一间小木屋，夏天挫冰吃瓜，冬天围炉取暖.</p>
+          </div>
+        </a>
+      </li>
+      <li class="mui-table-view-cell mui-media">
+        <a href="javascript:;">
+          <img class="mui-media-object mui-pull-left" src="../../static/images/1.jpg">
+          <div class="mui-media-body">
+            CBD
+            <p class='mui-ellipsis'>烤炉模式的城，到黄昏，如同打翻的调色盘一般.</p>
+          </div>
+        </a>
+      </li>
+    </ul>
   </div>
 </template>
 
 <script>
-export default {
-  
-}
+  import { Toast } from 'mint-ui'
+  export default {
+    data () {
+      return {
+        newsList: []
+      }
+    },
+    methods: {
+      getNewsList () {
+        this.$http.get('api/getnews?pageIdx=1').then(res => {
+          console.log(res)
+          if (res.body.status === 1) {
+            this.newsList = res.data.news
+          } else {
+            Toast('系统繁忙')
+          }
+        })
+      }
+    },
+    created () {
+      this.getNewsList()
+    }
+  }
 </script>
--- src/main.js
+++ src/main.js
@@ -4,6 +4,7 @@ import App from './App.vue'
 import MintUI from 'mint-ui'
 import VueRouter from 'vue-router'
 import router from '../src/router/router'
+import vueResource from 'vue-resource'
 
 import '../lib/bootstrap/css/bootstrap.min.css'
 import 'mint-ui/lib/style.css'
@@ -13,6 +14,10 @@ import '../public/style/main.scss'
 
 Vue.use(VueRouter)
 Vue.use(MintUI)
+Vue.use(vueResource)
+
+Vue.http.options.root = 'http://120.77.181.41:3000'
+Vue.http.options.emulateJSON = true
 
 let vm = new Vue({
   el: '#app',
